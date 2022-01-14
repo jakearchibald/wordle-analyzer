72101cc2094740e3788396382c053a09ad352ca1
Jake Archibald <jaffathecake@gmail.com>
2022-01-14 11:09:27 +0000

    Minor performance and correctness tweaks

--- node/index.ts
+++ node/index.ts
@@ -3,84 +3,113 @@ const require = createRequire(import.meta.url);
 const allWords = (require('./answers.json') as string[]).slice(0, 400);
 const answers = (require('./answers.json') as string[]).slice(0, 400);
 
-const eliminatedCounts = new Map<string, number[]>(
-  allWords.map((word) => [word, []]),
-);
-
-function answerIsValid(
+function possibleAnswer(
   answer: string,
-  knownParts: string[],
-  othersMustInclude: Set<string>,
+  positionalMatches: string[],
+  positionalNotMatches: string[],
+  additionalRequiredLetters: string[],
   mustNotContain: Set<string>,
 ): boolean {
+  const additionalRequiredLettersCopy = additionalRequiredLetters.slice();
+
   for (let i = 0; i < answer.length; i++) {
     const letter = answer[i];
+
+    if (positionalMatches[i]) {
+      if (positionalMatches[i] !== letter) return false;
+      // If we have an exact positional match, this letter is valid and no further checks are needed.
+      continue;
+    }
+
     if (
-      (knownParts[i] && letter !== knownParts[i]) ||
+      (positionalNotMatches[i] && letter === positionalNotMatches[i]) ||
       mustNotContain.has(letter)
     ) {
       return false;
     }
-    othersMustInclude.delete(letter);
+
+    const index = additionalRequiredLettersCopy.indexOf(letter);
+    if (index !== -1) additionalRequiredLettersCopy.splice(index, 1);
   }
 
   // It's valid if we've used up all the letters we need to
-  return othersMustInclude.size === 0;
+  return additionalRequiredLettersCopy.length === 0;
 }
 
-for (const answer of answers) {
-  const answerLetters = new Set(answer);
+function getBestAnswers() {
+  const eliminatedCounts = new Map<string, number[]>(
+    allWords.map((word) => [word, []]),
+  );
 
-  for (const guess of allWords) {
-    const remainingAnswerLetters = new Set(answer);
-    const knownParts = ['', '', '', '', ''];
-    const mustContain = new Set<string>();
-    const mustNotContain = new Set<string>();
+  for (const answer of answers) {
+    const answerLetters = new Set(answer);
 
-    for (let i = 0; i < guess.length; i++) {
-      const letter = guess[i];
+    for (const guess of allWords) {
+      const remainingAnswerLetters = new Set(answer);
+      const positionalMatches = ['', '', '', '', ''];
+      const positionalNotMatches = ['', '', '', '', ''];
+      const additionalRequiredLetters: string[] = [];
+      const mustNotContain = new Set<string>();
 
-      // If there's an exact positional match,
-      // This is a green square in Wordle.
-      if (answer[i] === letter) {
-        remainingAnswerLetters.delete(letter);
-        knownParts[i] = letter;
+      for (let i = 0; i < guess.length; i++) {
+        const letter = guess[i];
+
+        // If there's an exact positional match,
+        // This is a green square in Wordle.
+        if (answer[i] === letter) {
+          remainingAnswerLetters.delete(letter);
+          // Valid answers must also have this positional match
+          positionalMatches[i] = letter;
+        }
+        // Otherwise, if the answer contains the letter (and it hasn't already been matched).
+        // This is a yellow square in Wordle.
+        else if (remainingAnswerLetters.has(letter)) {
+          remainingAnswerLetters.delete(letter);
+          // Valid answers must not have this positional match, otherwise it'd be a green square
+          positionalNotMatches[i] = letter;
+          // Valid answers must contain this letter
+          additionalRequiredLetters.push(letter);
+        }
+        // The letter isn't in the answer.
+        // We don't check remainingAnswerLetters here, we only care if the answer doesn't contain the letters at all.
+        // This is when a keyboard key is greyed out in Wordle.
+        else if (!answerLetters.has(letter)) {
+          mustNotContain.add(letter);
+        }
       }
-      // Otherwise, if the answer contains the letter (and it hasn't already been matched).
-      // This is a yellow square in Wordle.
-      else if (remainingAnswerLetters.has(letter)) {
-        remainingAnswerLetters.delete(letter);
-        mustContain.add(letter);
+
+      let validAnswers = 0;
+
+      for (const answer of answers) {
+        if (
+          possibleAnswer(
+            answer,
+            positionalMatches,
+            positionalNotMatches,
+            additionalRequiredLetters,
+            mustNotContain,
+          )
+        ) {
+          validAnswers++;
+        }
       }
-      // The letter isn't in the answer.
-      // We don't check remainingAnswerLetters here, we only care if the answer doesn't contain the letters at all.
-      // This is when a keyboard key is greyed out in Wordle.
-      else if (!answerLetters.has(letter)) {
-        mustNotContain.add(letter);
-      }
+
+      // Record how many answers we eliminated for this guess.
+      eliminatedCounts.get(guess)!.push(answers.length - validAnswers);
     }
-
-    let validAnswers = 0;
-
-    for (const answer of answers) {
-      if (answerIsValid(answer, knownParts, mustContain, mustNotContain)) {
-        validAnswers++;
-      }
-    }
-
-    // Record how many answers we eliminated for this guess.
-    eliminatedCounts.get(guess)!.push(answers.length - validAnswers);
   }
+
+  const averageEliminatedCounts: [string, number][] = [];
+
+  for (const [guess, eliminated] of eliminatedCounts) {
+    averageEliminatedCounts.push([
+      guess,
+      eliminated.reduce((a, b) => a + b, 0) / eliminated.length,
+    ]);
+  }
+
+  averageEliminatedCounts.sort((a, b) => b[1] - a[1]);
+  console.log(averageEliminatedCounts.slice(0, 10));
 }
 
-const averageEliminatedCounts: [string, number][] = [];
-
-for (const [guess, eliminated] of eliminatedCounts) {
-  averageEliminatedCounts.push([
-    guess,
-    eliminated.reduce((a, b) => a + b, 0) / eliminated.length,
-  ]);
-}
-
-averageEliminatedCounts.sort((a, b) => b[1] - a[1]);
-console.log(averageEliminatedCounts.slice(0, 10));
+getBestAnswers();
