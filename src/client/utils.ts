9975ba85c73034bd6712d99809ac7f3e35a3138e
Jake Archibald <jaffathecake@gmail.com>
2022-02-11 15:31:01 +0000

    Avoid breakage in Firefox private browsing mode

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -12,6 +12,8 @@ function onControllerChange() {
 
 export async function addServiceWorker() {
   if (!__PRODUCTION__) return;
+  // Firefox in private browsing doesn't support service workers
+  if (!navigator.serviceWorker) return;
   navigator.serviceWorker.register(swURL);
   let hadPreviousController = !!navigator.serviceWorker.controller;
 
@@ -31,12 +33,16 @@ export async function addServiceWorker() {
 }
 
 export async function swUpdatePending(): Promise<boolean> {
+  // Firefox in private browsing doesn't support service workers
+  if (!navigator.serviceWorker) return false;
   const reg = await navigator.serviceWorker.getRegistration();
   if (!reg) return false;
   return !!reg.waiting;
 }
 
 export async function activatePendingSw(): Promise<void> {
+  // Firefox in private browsing doesn't support service workers
+  if (!navigator.serviceWorker) return;
   const reg = await navigator.serviceWorker.getRegistration();
   if (!reg || !reg.waiting) throw Error('No pending service worker');
   reg.waiting.postMessage('skipWaiting');
