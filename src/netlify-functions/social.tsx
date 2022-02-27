5132b00115e2d36b4c53993860d822c757bb26d8
Jake Archibald <jaffathecake@gmail.com>
2022-02-27 08:08:31 -0500

    Rendering to PNG

--- src/netlify-functions/social.tsx
+++ src/netlify-functions/social.tsx
@@ -1,12 +1,17 @@
 import { h } from 'preact';
-import render from 'preact-render-to-string';
+import renderToString from 'preact-render-to-string';
 import { Handler } from '@netlify/functions';
+import { render } from '@resvg/resvg-js';
+
 import SocialSVG from 'shared/SocialSVG';
 
 export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'image/svg+xml; charset=utf-8' },
-    body: render(<SocialSVG entries={[]} />),
+    headers: { 'Content-Type': 'image/png' },
+    isBase64Encoded: true,
+    body: render(renderToString(<SocialSVG entries={[]} />), {}).toString(
+      'base64',
+    ),
   };
 };
