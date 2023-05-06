0f5458c7bc70af9baa48b957166867201f638234
jakearchibald <jaffathecake@gmail.com>
2023-05-06 09:17:00 +0100

    Reinstate transitions

--- src/client/App/lazy-app/transitions.ts
+++ src/client/App/lazy-app/transitions.ts
@@ -9,7 +9,7 @@ export async function performToAnalysisTransition(
   updateDOM: TransitionHelperArg['updateDOM'],
 ) {
   // Early exit to avoid browsers throwing when they try to assignStyles to pseudos they're not familiar with.
-  if (!('startViewTransition' in document) || true) {
+  if (!('startViewTransition' in document)) {
     updateDOM();
     return;
   }
